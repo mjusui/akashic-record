91f413db5d943c2a2160973927676fdd7dae615b
mjusui <mjusui@gmail.com>
2025-06-12 13:42:36 +0000

    select endpoint

--- side_panel/index.js
+++ side_panel/index.js
@@ -4,6 +4,7 @@
   const onclick=(ev)=>{
     const { target, }=ev;
     const { cmd, }=target.dataset;
+    console.log(cmd, ':', ev);
 
     if(cmd === 'click/get-manhours'){
       console.log('click/get-manhours:', ev);
@@ -12,4 +13,17 @@
     }
   };
   root.addEventListener('click', onclick);
+
+  let endpoint='https://atnd.ak4.jp';
+  const onchange=(ev)=>{
+    const { target, }=ev;
+    const { cmd, }=target.dataset;
+    console.log(cmd, ':', ev);
+
+    if(cmd === 'change/set-endpoint'){
+      endpoint=target.value; 
+      return;
+    }
+  };
+  root.addEventListener('change', onchange);
 })();
