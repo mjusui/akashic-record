e418326f43ca8117268826aa0b45efe93d2cab0f
mjusui <mjusui@gmail.com>
2025-06-12 15:03:34 +0000

    save common settings

--- side_panel/index.js
+++ side_panel/index.js
@@ -1,4 +1,5 @@
 (()=>{
+
   const root=document.getElementById('root');
 
   const textarea_kosu_result=document.getElementById('textarea-kosu-result');
@@ -23,8 +24,16 @@
     const { cmd, }=target.dataset;
     console.log(cmd, ':', ev);
 
-    if(cmd === 'change/set-endpoint'){
-      endpoint=target.value; 
+    if(cmd === 'change/save-endpoint'){
+      localStorage.setItem('common/endpoint', target.value);
+      return;
+    }
+    if(cmd === 'change/save-token'){
+      localStorage.setItem('common/token', target.value);
+      return;
+    }
+    if(cmd === 'change/save-coopid'){
+      localStorage.setItem('common/coopid', target.value);
       return;
     }
   };
@@ -33,6 +42,25 @@
   const select_endpoint=document.getElementById('select-endpoint');
   const input_token=document.getElementById('input-token');
   const input_coopid=document.getElementById('input-coopid');
+
+  ['endpoint', 'token', 'coopid', ].forEach(key =>{
+    const val=localStorage.getItem(`common/${key}`);
+    if(!val){
+      return;
+    }
+    if(key === 'endpoint'){
+      [...select_endpoint.options,].forEach(option =>{
+        option.value === val && option.selected=true;
+      });
+    }
+    if(key === 'token'){
+      input_token.value=val;
+    }
+    if(key === 'coopid'){
+      input_coopid.value=val;
+    }
+  });
+
   const request=(hndl, ctxt)=>{
     if( !input_token.reportValidity() ){
       return;
