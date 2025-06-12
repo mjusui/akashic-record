4a8b985f87873c8c8729e1a0193d1ddfb37618b0
mjusui <mjusui@gmail.com>
2025-06-12 13:29:26 +0000

    fix console.log

--- side_panel/index.js
+++ side_panel/index.js
@@ -1,13 +1,12 @@
 (()=>{
   const root=document.getElementById('root');
-  const msg=document.getElementById('msg');
 
   const onclick=(ev)=>{
     const { target, }=ev;
     const { cmd, }=target.dataset;
 
     if(cmd === 'click/get-manhours'){
-      msg.textContent='click/get-manhours';
+      console.log('click/get-manhours:', ev);
       return;
     }
   };
